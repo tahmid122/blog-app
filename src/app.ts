133ffc0c71f90d861fc540141adfe5cf56cf0f6d
Tahmid Alam <mdtahmidalam122@gmail.com>
2026-07-30 16:33:07 +0600

    get all posts done

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application, Request, Response } from "express";
 import { postRoutes } from "./modules/post/post.routes";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 const app: Application = express();
+
+app.all("/api/auth/*", toNodeHandler(auth));
 
 //default middlewares
 app.use(express.json());
